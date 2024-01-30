bcf5c294ee07bb71766e5cde486cbc51ad7e0ec2
Brendan Kormos <brendaaank@gmail.com>
2024-01-30 19:10:30 +0000

    initial database commit

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,45 @@
 set client_min_messages to warning;
 
 -- DANGER: this is NOT how to do it in the real world.
--- `drop schema` INSTANTLY ERASES EVERYTHING.
+-- drop schema INSTANTLY ERASES EVERYTHING.
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+  "userId" serial PRIMARY KEY,
+  "username" varchar,
+  "hashedPassword" varchar,
+  "createdAt" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "public"."projects" (
+  "projectId" serial PRIMARY KEY,
+  "title" varchar,
+  "ownerId" integer,
+  "boardId" integer,
+  "createdAt" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "public"."boards" (
+  "boardId" serial PRIMARY KEY,
+  "projectId" integer,
+  "title" varchar,
+  "description" varchar,
+  "createdAt" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "public"."boardObjects" (
+  "boardObjectId" serial PRIMARY KEY,
+  "boardId" integer,
+  "x" integer,
+  "y" integer,
+  "type" varchar,
+  "createdAt" timestamp DEFAULT (now())
+);
+
+ALTER TABLE "public"."projects" ADD FOREIGN KEY ("ownerId") REFERENCES "public"."users" ("userId");
+
+ALTER TABLE "public"."boards" ADD FOREIGN KEY ("projectId") REFERENCES "public"."projects" ("projectId");
+
+ALTER TABLE "public"."boards" ADD FOREIGN KEY ("boardId") REFERENCES "public"."boardObjects" ("boardId");
