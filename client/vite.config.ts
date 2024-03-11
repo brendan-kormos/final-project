31a46242627fe2a95205a0bc281f6a4c0b99de91
Brendan Kormos <brendaaank@gmail.com>
2024-03-11 22:11:33 +0000

    testing2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: { chunkSizeWarningLimit: 650 },
-
   server: {
     proxy: {
       '/api': 'http://localhost:8080',
