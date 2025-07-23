896acea7d91f7343dbbe17ad0166bd93ab41c044
brendan-kormos <brendank501@gmail.com>
2025-07-23 00:16:10 +0000

    remove excess comment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// https://stackoverflow.com/questions/71763113/how-to-change-vite-application-port
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
