92cd3e90152c35fc507b9ef82bedb8486fb66a9a
brendan-kormos <brendank501@gmail.com>
2025-07-23 00:14:27 +0000

    fixed npm-run-dev doesn't work #6

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+// https://stackoverflow.com/questions/71763113/how-to-change-vite-application-port
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       '/api': 'http://localhost:8080',
     },
